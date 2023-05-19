31fa5b3264be0d8e489a72d80840bbca7b2ef948
mohsenebrahimyir <mohsenebrahimy.ir@gmail.com>
2023-05-19 13:01:04 +0330

    add get async api

--- src/codal_tsetmc/tools/api.py
+++ src/codal_tsetmc/tools/api.py
@@ -3,8 +3,11 @@ from urllib.request import urlopen
 import requests
 import pandas as pd
 
+import asyncio
+import aiohttp
 
 def get_dict_from_xml_api(url: str) -> dict:
+ 
     try:
         with urlopen(url) as file:
             string_json = file.read().decode('utf-8')
@@ -20,17 +23,10 @@ def get_dict_from_xml_api(url: str) -> dict:
 
 def get_data_from_cdn_tsetmec_api(data: str, code: str, date: str):
     url = f'http://cdn.tsetmc.com/api/{data}/{code}/{date}'
-    cookies = {}
-
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
     }
-    response = requests.get(
-        url=url,
-        cookies=cookies,
-        headers=headers,
-        verify=False
-    )
+    response = requests.get(url=url, cookies={}, headers=headers, verify=False)
     
     return response.json()
 
@@ -39,3 +35,9 @@ def get_rawdata_from_github(symbol):
     url = f"https://raw.githubusercontent.com/mohsenebrahimyir/codal-tsetmc/master/data/{symbol}.csv"
     df = pd.read_csv(url)
     return df
+
+
+async def get_async_api(url) -> dict:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response
